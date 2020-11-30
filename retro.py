e0c0478117e9ad2e8c45bac28e1de8ab973216e2
Alt Code <64210720+AltCode@users.noreply.github.com>
2020-11-30 14:34:55 +0600

    Add files via upload

---
+++ retro.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.online, activity=discord.Game('ReTro'))
+    print ("ReTro is ready")
+    
+client.run(os.environ['DISCORD_TOKEN'])
