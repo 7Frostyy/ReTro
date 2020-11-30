5f5ebd30c38e23fecb8a1effc9f6c6e8c6e1c65d
Alt Code <64210720+AltCode@users.noreply.github.com>
2020-11-30 15:18:07 +0600

    Update retro.py

--- retro.py
+++ retro.py
@@ -6,7 +6,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game('ReTro'))
+    await client.change_presence(activity=discord.Game('ReTro by AltCode'))
     print ("ReTro is ready")
     
 client.run(os.environ['DISCORD_TOKEN'])
